c7c0339c8bece2ae73c4f9a1f0cb6ea43ff39077
timwappat <twappat@iee.org>
2016-12-23 10:58:26 +0000

    Init

---
+++ ProtocolHandler/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace ProtocolHandler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start(GenerateURI(textBox2.Text));
+        }
+
+        private string GenerateURI(string Template)
+        {
+            return string.Format(Template, textBox1.Text);
+        }
+      
+    }
+}
